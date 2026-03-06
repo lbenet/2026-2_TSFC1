b7fdf04833333efdb5e6448396fcc95997391e69
Ale Hdz <ale.hdz.43@proton.me>
2026-03-05 23:36:53 -0600

    Añado correciones  y comentarios del ejercicio 1, completando incisos a. y c.

--- Tareas/Alejandra_Hernandez_Y_Ivan_Marquez/Tarea1.jl
+++ Tareas/Alejandra_Hernandez_Y_Ivan_Marquez/Tarea1.jl
@@ -1,8 +1,7 @@
 # Tarea 1 
 
 # ## 1. Triángulo de Pascal
-using Pkg
-Pkg.add("Plots")
+using Plots
 #
 # a. Generen el triángulo de Pascal, hasta cierto orden `ord`, usando una
 # matriz de enteros, en la que cada renglón representa un orden distinto.
@@ -10,20 +9,51 @@ Pkg.add("Plots")
 #
 
 """
-    function trianguloPascal(ord:Int)
+    function trianguloPascal(ord::Int)
 
 Genera una matriz del triángulo de Pascal, hasta un orden dado
 """
-#function trianguloPascal(ord::Int)
-    ord = 4
-    triangulo = zeros(Int,ord,ord)
-    #for i = ord
-    #   triangulo[i]
-    
+function trianguloPascal(ord::Int)
+    triang = zeros(Int,ord,ord) # Creamos una matriz cuadrada de ceros
+    j = ceil(Int,ord/2) # Establecemos una variable en el "centro" de la matriz
+    for i = 1:ord #Primero ponemos los 1s de la izquierda
+        triang[i,j] = 1
+        if mod(i,2) == 0
+            j -= 1
+        end
+    end
 
-    #return triangulo
-#end
+    j = ceil(Int,ord/2) # Reestablecemos la variable central
+    for i = 1:ord # Ahora colocamos los 1s de la derecha
+        triang[i,j] = 1
+        if mod(i,2) != 0
+            j+= 1
+        end
+    end
 
+    j = ceil(Int,ord/2)  # Colocamos un "indicador" a la mitad de la matriz
+    k = ceil(Int,ord/2) # Y una variable que vaya recorriendose cada dos lineas para ir llenando la matriz
+    i = 1   # Y una variable más para ir bajando cada fila
+    while i <= ord
+        if triang[i,j] == 0 && mod(i,2) !=0 # Si nuestra variable es 0 y está en una fila impar, sumamos el número de arriba y el que está a la derecha
+            triang[i,j] = (triang[i-1,j])+(triang[i-1,j+1])
+            j += 1 # Y nos recorremos una posición a la derecha
+        elseif triang[i,j] == 0 && mod(i,2) == 0 # Similar a lo de arriba, pero con la fila par, sumando el número de arriba y el de su izquierda
+            triang[i,j] = (triang[i-1,j-1])+(triang[i-1,j])
+            j += 1 #También nos recorremos una posición a la derecha
+        elseif triang[i,j] == 1 # Si la variable vale 1, llegamos al final de la fila
+            i += 1 # Bajamos a la fila que sigue
+            if i > 4 && mod(i,2) != 0 # Y a partir de la tercera fila, cada dos filas empezamos a ir recorriendo nuestro punto inicial uno a la izquierda
+                k -= 1
+            end
+            j = k # Reiniciamos nuestra posición inicial para comenzar con la siguiente fila
+        end
+    end
+
+    return triang
+end
+
+trianguloPascal(9)
 # b. Usen la matriz creada para generar *otra*, en que todo>s los números
 # pares aparezcan como `false` y los impares como `true`, o alternativamente
 # como 0 y 1, respectivamente. Las funciones `isodd` o `iseven` pueden
@@ -34,29 +64,38 @@ Toma una matriz y regresa otra, reemplazando los números impares con 1 y los pa
 """
 function imparYPar(mat::Matrix)
     A = copy(mat) #Creamos una copia de matriz original, que modificaremos
-    for i = 1:length(A)
+    for i in eachindex(A)
         A[i] = isodd(A[i])
     end # Para cada entrada en la matriz reemplazamos los impares con 1 y los pares con 0
     return A # La función regresa la matriz modificada
 end
-#
+
 # c. Dibujen (con puntos) todos los valores impares del triángulo de Pascal
 # para `ord=256`. Repitan el ejercicio para `ord=1024`.
+"""
 function dibujarMatriz(mat::Matrix)
-    x = Vector{Int}(undef,0)
+    Toma una matriz de 0s y 1s, y crea una gráfica donde se muestren los 1 como puntos y los 0 
+    como espacio vacio
+"""
+function dibujarMatriz(mat::Matrix)
+    x = Vector{Int}(undef,0) #Creamos vectores vacios para nuestros x y y
     y = Vector{Int}(undef,0)
-    tamano = size(matPrueba2)
-    mat = copy(matPrueba2)
-    for i = 1:tamano[1]
-        for j = 1:tamano[2]
-            if mat[i,j] == 1
-                push!(x,i)
+    tam = size(mat) # Establecemos un vector que tiene las dimensiones de la matriz 
+    mat = copy(mat)
+    for i = 1:tam[1] # Para iterar en todas las filas
+        for j = 1:tam[2] # Para iterar en todas las columnas
+            if mat[i,j] == 1 # Busca que el valor en la matriz sea igual a 1
+                push!(x,i)  #Si lo es, lo coloca en nuestros vectores x y y
                 push!(y,j)
             end
         end
     end
+    graf = scatter(x,y) #Finalmente, grafica los vectores
+    return graf
 end
 
+orden256 = dibujarMatriz(imparYPar(trianguloPascal(256)))
+orden1024 = dibujarMatriz(imparYPar(trianguloPascal(1024)))
 # ## 2. Triángulo equilátero
 #
 # Sean $X_1$, $X_2$ y $X_3$ los vértices de un triángulo equilátero en el
@@ -78,7 +117,6 @@ end
 # e. Grafiquen todos los iterados $Y_n$ que guardaron, considerando
 # muuuuchos puntos.
 
-using Plots 
 
 
 #Para simplificar las cosas, fijaré dos de los puntos al eje X con v[1]<u[1]
