936d30098d487c4d25f7bf7744112fe0e73c6e99
Adrian Arriaga <adrian.arriaga.a98@gmail.com>
2026-04-28 11:10:38 -0600

    Se corrigen las observaciones sobre  el nombre del struct, el test de la función potencia con n=2, se sobrecarga la función isapprox y se incluye un nuevo test entre duales y otro para fun( ) y der( ).

--- Tareas/Adrian_Tareas/AdrianArriaga_Tarea2.jl
+++ Tareas/Adrian_Tareas/AdrianArriaga_Tarea2.jl
@@ -7,42 +7,42 @@ using Test
 # a. Se define la estructura de los numeros duales, con los campos 'fun' y 'der', los cuales  
 # son subtipos del campo 'Real'. 
 
-struct DualNumb{T<:Real}
+struct Dual{T<:Real}
     fun::T
     der::T
 end
 
 #b. Se sobrecargan las operaciones aritméticas y funciones elementales para los Duales.
-import Base: +, -, *, /, ^, sin, cos, tan, exp, log, sqrt, sinh, cosh
+import Base: +, -, *, /, ^, sin, cos, tan, exp, log, sqrt, sinh, cosh,  isapprox
 
 # Se sobrecargan las operaciones aritméticas.
-function +(a::DualNumb, b::DualNumb)
-    c = DualNumb(a.fun + b.fun, a.der + b.der)
+function +(a::Dual, b::Dual)
+    c = Dual(a.fun + b.fun, a.der + b.der)
     return c
 end
 
-function -(a::DualNumb, b::DualNumb)
-    c = DualNumb(a.fun - b.fun, a.der - b.der)
+function -(a::Dual, b::Dual)
+    c = Dual(a.fun - b.fun, a.der - b.der)
     return c
 end
 
-function *(a::DualNumb, b::DualNumb)
-    c = DualNumb((a.fun)*(b.fun), (a.fun)*(b.der) + (a.der)*(b.fun))
+function *(a::Dual, b::Dual)
+    c = Dual((a.fun)*(b.fun), (a.fun)*(b.der) + (a.der)*(b.fun))
     return c
 end
 
 
-function /(a::DualNumb, b::DualNumb)
+function /(a::Dual, b::Dual)
     local x = a.fun
     local y = a.der
     local u = b.fun
     local w = b.der
-    c = DualNumb((x)*(u^(-1)), ((y)*(u) - (x)*(w))*(u^(-2)))
+    c = Dual((x)*(u^(-1)), ((y)*(u) - (x)*(w))*(u^(-2)))
     return c
 end
 
-function ^(a::DualNumb, n::Int64)
-    c = DualNumb((a.fun)^n, n*(a.fun^(n-1))*(a.der))
+function ^(a::Dual, n::Int64)
+    c = Dual((a.fun)^n, n*(a.fun^(n-1))*(a.der))
     return c
 end
 
@@ -51,67 +51,67 @@ end
 # un sólo valor (en lugar de los dos requeridos). Esto corresponde a
 # $\mathbb{D}_{x_0}c = (c, 0)$, donde $c$ es una constante (real).
 
-function DualNumb(u::Real)
-    c = DualNumb(u, zero(u))
+function Dual(u::Real)
+    c = Dual(u, zero(u))
     return c
 end
 
-dd = DualNumb(5)
+dd = Dual(5)
 
 
 # d. Se extienden los métodos que permitan sumar/restar y multiplicar/dividir un
 # número (`::Real`) y un `::Dual`. 
 
-function +(a::Real, b::DualNumb)
-    c = DualNumb(a) + b
+function +(a::Real, b::Dual)
+    c = Dual(a) + b
     return c
 end
 
-function +(a::DualNumb, b::Real)
-    c = a + DualNumb(b)
+function +(a::Dual, b::Real)
+    c = a + Dual(b)
     return c
 end
 
 
-function -(a::Real, b::DualNumb)
-    c = DualNumb(a) - b
+function -(a::Real, b::Dual)
+    c = Dual(a) - b
     return c
 end
 
-function -(a::DualNumb, b::Real)
-    c = a - DualNumb(b)
+function -(a::Dual, b::Real)
+    c = a - Dual(b)
     return c
 end
 
-function *(a::Real, b::DualNumb)
-    c = DualNumb(a)*b
+function *(a::Real, b::Dual)
+    c = Dual(a)*b
     return c
 end
 
-function *(a::DualNumb, b::Real)
-    c = a*DualNumb(b)
+function *(a::Dual, b::Real)
+    c = a*Dual(b)
     return c
 end
 
-function /(a::Real, b::DualNumb)
-    c = DualNumb(a)/b
+function /(a::Real, b::Dual)
+    c = Dual(a)/b
     return c
 end
 
-function /(a::DualNumb, b::Real)
-    c = a/DualNumb(b)
+function /(a::Dual, b::Real)
+    c = a/Dual(b)
     return c
 end
 
 # e. Se definen las funciones `fun` y `der` que, al ser aplicadas a un `Dual` devuelven
 # la parte que corresponde a la función y la parte que corresponde a la derivada
 # del `Dual`, respectivamente.
-function fun(u::DualNumb)
+function fun(u::Dual)
     c = u.fun;
     return c
 end
 
-function der(u::DualNumb)
+function der(u::Dual)
     c = u.der;
     return c
 end
@@ -125,21 +125,22 @@ end
 
 @testset "Test dual arithmetic" begin
     
-    A = DualNumb(1.0, 3.0)
-    B = DualNumb(2.0, 1.0)
+    A = Dual(1.0, 3.0)
+    B = Dual(2.0, 1.0)
+    n=2
 
     # Pruebas de aritmética básica: 
-    @test A + B == DualNumb(3.0, 4.0)
-    @test A - B == DualNumb(-1.0, 2.0)
-    @test A * B == DualNumb(2.0, 7.0)
-    @test A / B == DualNumb(0.5, 1.25)
+    @test A + B == Dual(3.0, 4.0)
+    @test A - B == Dual(-1.0, 2.0)
+    @test A * B == Dual(2.0, 7.0)
+    @test A / B == Dual(0.5, 1.25)
 
     # Probando operaciones mixtas: 
-    @test A^2 == DualNumb(1.0, 6.0) 
-    @test A * 2.0 == DualNumb(2.0, 6.0) 
+    @test A^n == Dual(1.0, 6.0) 
+    @test A * 2.0 == Dual(2.0, 6.0) 
     
     # Probando el generador de Duales: 
-    @test DualNumb(2.0) == DualNumb(2.0, 0.0)
+    @test Dual(2.0) == Dual(2.0, 0.0)
 
     #Probando las funciones fun() y der():
     @test fun(A) == 1.0
@@ -161,7 +162,7 @@ end
 
 function dual(x_0)
     a, b = promote(x_0, one(x_0))
-    c = DualNumb(a, b)
+    c = Dual(a, b)
     return c
 end
 
@@ -177,61 +178,72 @@ f(dual(2//1))
 # sencilla, que lo que hicieron da lo que uno esperaría obtener.
 
 #Se sobrecargan las funciones elementales:
-function sin(a::DualNumb)
-    c = DualNumb(sin(fun(a)), cos(fun(a))*der(a))
+function sin(a::Dual)
+    c = Dual(sin(fun(a)), cos(fun(a))*der(a))
     return c
 end
 
-function cos(a::DualNumb)
-    c = DualNumb(cos(fun(a)), -sin(fun(a))*der(a))
+function cos(a::Dual)
+    c = Dual(cos(fun(a)), -sin(fun(a))*der(a))
     return c
 end
 
-function tan(a::DualNumb)
-    c = DualNumb(tan(fun(a)), (sec(fun(a))^2)*der(a))
+function tan(a::Dual)
+    c = Dual(tan(fun(a)), (sec(fun(a))^2)*der(a))
     return c
 end
 
-function log(a::DualNumb)
-    c = DualNumb(log(fun(a)), der(a)/fun(a))
+function log(a::Dual)
+    c = Dual(log(fun(a)), der(a)/fun(a))
     return c
 end
 
-function exp(a::DualNumb)
-    c = DualNumb(exp(fun(a)), exp(fun(a))*der(a))
+function exp(a::Dual)
+    c = Dual(exp(fun(a)), exp(fun(a))*der(a))
     return c
 end
 
-function sqrt(a::DualNumb)
-    c = DualNumb(sqrt(fun(a)), (0.5)*(fun(a)^(-0.5))*der(a))
+function sqrt(a::Dual)
+    c = Dual(sqrt(fun(a)), (0.5)*(fun(a)^(-0.5))*der(a))
     return c
 end
 
 
-function sinh(a::DualNumb)
-    c = DualNumb(sinh(fun(a)), cosh(fun(a))*der(a))
+function sinh(a::Dual)
+    c = Dual(sinh(fun(a)), cosh(fun(a))*der(a))
     return c
 end
 
-function cosh(a::DualNumb)
-    c = DualNumb(cosh(fun(a)), sinh(fun(a))*der(a))
+function cosh(a::Dual)
+    c = Dual(cosh(fun(a)), sinh(fun(a))*der(a))
     return c
 end
+
+
+function isapprox(a::Dual, b::Dual; atol=1e-8, rtol=0.0)
+    return isapprox(fun(a), fun(b), atol=atol, rtol=rtol) && isapprox(der(a), der(b), atol=atol, rtol=rtol)
+end
+
 
 @testset "Testing elemental functions over dual numbers" begin
     
     #Se crea un dual evaluado en x_0 = π:
 
-    @test dual(pi) == DualNumb(Float64(pi), 1.0)
+    @test dual(pi) == Dual(Float64(pi), 1.0)
+
 
     x_pi = dual(π) # Equivale a evaluar en x = pi
     x_zero = dual(0.0)
     x_one = dual(1.0) # Equivale a evaluar en x = 0
+
+    @test fun(x_pi) == Float64(pi)
+    @test der(x_pi) == Float64(1.0)
     
     # Derivada de sen(x) es cos(x). En x=pi, cos(pi) = -1. 
     # Usamos ≈ porque pi tiene infinitos decimales.
     @test der(exp(x_zero)) ≈ 1.0 
     @test der(sin(x_pi)) ≈ -1.0 
+    @test sin(x_pi) ≈ Dual(0.0, -1.0)
     @test der(cos(dual(0.5*π))) ≈ -1.0 
     @test der(tan(x_zero)) ≈ 1.0 
     @test der(tan(x_one)) ≈ 3.425518820814759
